db297fa541f9f07df59cbf2a4f24a05683388ff7
Eduardo Ballesteros <eiballesterosm@unadvirtual.edu.co>
2019-12-05 23:25:38 -0500

    FEATURE: JS Programming for Every one project

---
+++ JSProgrammingForEveryone/Scripts/mathflashcards.js
@@ -0,0 +1,30 @@
+﻿function GenerateNumbers() {
+    var numOne = Math.floor(Math.random() * 10) + 1;
+    var numTwo = Math.floor(Math.random() * 10) + 1;
+    var correctAnswer = numOne + numTwo;
+}
+
+function Addition() {
+    var ourAnswer = document.getElementById("answer").value;
+    if (!isNaN(ourAnswer)) {
+        //document.getElementById("output").innerHTML = ourAnswer;
+        if (ourAnswer == correctAnswer) {
+            document.getElementById("output").innerHTML = "Correct!!! " + numOne + " + " + numTwo + " = " + correctAnswer;
+        }
+        else {
+            document.getElementById("output").innerHTML = "Incorrect";
+        }
+    }
+    else {
+        document.getElementById("output").innerHTML = "Our answer is not a number!!!!";
+    }
+}
+
+function NewCard() {
+    document.getElementById("output").innerHTML = "";
+    document.getElementById("answer").value = "";
+    numOne = Math.floor(Math.random() * 10) + 1;
+    numTwo = Math.floor(Math.random() * 10) + 1;
+    correctAnswer = numOne + numTwo;
+    document.getElementById("title").innerHTML = numOne + " + " + numTwo;
+}
